a79bb0cb3ff981a4dea2dde8cd3bafa71f769fa8
jcomartins <jose.martins@agxeed.com>
2026-02-20 16:22:35 +0100

    merge upstream

--- src/nebula_velodyne/nebula_velodyne_hw_interfaces/src/velodyne_hw_interface.cpp
+++ src/nebula_velodyne/nebula_velodyne_hw_interfaces/src/velodyne_hw_interface.cpp
@@ -107,20 +107,16 @@ Status VelodyneHwInterface::get_sensor_configuration(SensorConfigurationBase & s
 
 VelodyneStatus VelodyneHwInterface::init_http_client()
 {
-  while (true) {
-    try {
-      http_client_driver_->init_client(sensor_configuration_->sensor_ip, 80);
-      if (!http_client_driver_->client()->isOpen()) {
-        http_client_driver_->client()->open();
-      }
-      return Status::OK;
-    } catch (const std::exception & ex) {
-      RCLCPP_INFO_ONCE(
-        *parent_node_logger_, "Cannot connect to lidar because of: %s. Will keep trying...",
-        ex.what());
-      std::this_thread::sleep_for(std::chrono::seconds(5));
+  try {
+    http_client_driver_->init_client(sensor_configuration_->sensor_ip, 80);
+    if (!http_client_driver_->client()->isOpen()) {
+      http_client_driver_->client()->open();
     }
+  } catch (const std::exception & ex) {
+    VelodyneStatus status = Status::HTTP_CONNECTION_ERROR;
+    return status;
   }
+  return Status::OK;
 }
 
 void VelodyneHwInterface::string_callback(const std::string & str)
