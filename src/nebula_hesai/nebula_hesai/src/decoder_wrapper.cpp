b7c43413da15e6c93590727570a5809f548a008b
jcomartins <jose.martins@agxeed.com>
2026-04-08 12:53:11 +0200

    change timestamp source (hesai)

--- src/nebula_hesai/nebula_hesai/src/decoder_wrapper.cpp
+++ src/nebula_hesai/nebula_hesai/src/decoder_wrapper.cpp
@@ -176,7 +176,7 @@ void HesaiDecoderWrapper::on_pointcloud_decoded(
     cloud_watchdog_->update();
   }
 
-  rclcpp::Time cloud_stamp = current_scan_msg_->header.stamp;
+  rclcpp::Time cloud_stamp = parent_node_.get_clock()->now();
 
   // Publish scan message only if it has been written to
   if (current_scan_msg_ && !current_scan_msg_->packets.empty() && packets_pub_thread_) {
